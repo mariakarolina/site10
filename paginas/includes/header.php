182fe1a521f934714bb6a534c874134be6cdb440
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-7TUSQ40>
2021-01-06 11:38:31 -0300

    arquivos base

---
+++ paginas/includes/header.php
@@ -0,0 +1,103 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title> contato</title>
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="plugins/IcoMoon/iconmoon.css.css">
+        <link rel="stylesheet" href="/assets/css/contato.css">
+    </head>
+    <body>
+        <header class="contanier">   
+            <!--slide-->
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="img/1911.plantasrelaxantes7-1280x720.jpg" alt="Primeiro Slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="img/dois.jpg" alt="Segundo Slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="img/confira-dicas-de-como-cultivar-plantas-em-casa-1280x720.jpg" alt="Terceiro Slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Anterior</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Próximo</span>
+                </a>
+            </div>
+            <!--slide-->
+
+
+
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#"Inicio</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="?pg=lpgin">Produto</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="?pg=lpgin">Fale Conosco</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="?pg=login">login</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="?pg=login2">login2</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="?pg=login3">login3</a>
+                        </li>
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Redes Sociais
+
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="">
+                            <a class="dropdown-item" href="#">facebook</a>
+                            <a class="dropdown-item" href="#">Instagram</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">twitter</a>
+                        </div>
+
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Desativado</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+                    </form>
+                    
+                </div>
+            </nav>
+            <div class="pos-f-t">
+                <div class="collapse" id="navbarToggleExternalContent">
+                    <div class="bg-dark p-4">
+                        <h5 class="text-white h4">Conteúdo expandido</h5>
+                        <span class="text-muted">Expansível, atráves da marca no navbar.</span>
+                    </div>
+                </div>
+                <nav class="navbar navbar-dark bg-dark">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Alterna navegação">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </nav>
+            </div> 
+        </header>
+
